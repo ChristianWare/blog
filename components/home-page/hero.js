1a01322b3fabf063f42d7182f27e73bea64aef28
Chris <chris.ware.dev@gmail.com>
2021-09-08 14:40:21 -0700

    Hero component with styling

---
+++ components/home-page/hero.js
@@ -0,0 +1,25 @@
+import Image from "next/image";
+
+import classes from "./hero.module.css";
+
+function Hero() {
+  return (
+    <section className={classes.hero}>
+      <div className={classes.image}>
+        <Image
+          src='/images/site/black.png'
+          alt='An image showing Chris'
+          width={300}
+          height={300}
+        />
+      </div>
+      <h1>What's Up, I go by the name of Black Though</h1>
+      <p>
+        I'm the lead singer/rapper in my band, The Roots. Watch me on The
+        Tonight Show with Jimmy Fallon.{" "}
+      </p>
+    </section>
+  );
+}
+
+export default Hero;
